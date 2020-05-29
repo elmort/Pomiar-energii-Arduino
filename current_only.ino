1cd5fb4d6c21ad885b8982e0625ae9588fd1c213
Marcin <marcin@elmort.pl>
2020-05-29 19:36:21 +0200

    Prawidłowy plik ze zmianą wartości korygującej

---
+++ current_only.ino
@@ -0,0 +1,21 @@
+// EmonLibrary examples openenergymonitor.org, Licence GNU GPL V3
+
+#include "EmonLib.h"                   // Include Emon Library
+EnergyMonitor emon1;                   // Create an instance
+
+void setup()
+{  
+  Serial.begin(9600);
+  
+  emon1.current(1, 33.1);             // Current: input pin, calibration.
+                                      //zmieniona wartośc w stosunku do oryginalnego szkicu przy rezystorze na wyjściu czujnika 460 om
+}
+
+void loop()
+{
+  double Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  
+  Serial.print(Irms*230.0);	       // Apparent power
+  Serial.print(" ");
+  Serial.println(Irms);		       // Irms
+}
